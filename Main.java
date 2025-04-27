13a4596ba64fcb433d014605be8373d174715abb
GovindS2025 <shwetworldtechnologies@gmail.com>
2025-04-27 21:26:14 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,45 +2,40 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-                /* Hi there in this episode we will print a number from series to one single line
-                * ex: 57585
-                * o/p : 5
-                * 7
-                * 5
-                * 8
-                * 5
-                *
-                *
-                *
-                *  */
-        Scanner sc = new Scanner(System.in);
-        int num= sc.nextInt();
+        /*
+         * 📌 Program: Print each digit of a number from left to right
+         * 🎯 Example:
+         * Input: 57585
+         * Output:
+         * 5
+         * 7
+         * 5
+         * 8
+         * 5
+         */
 
-        int numOfDig =0;
-        int temp = num;
-        while(temp !=0){
-            temp = temp/10;  //so it is reducing a dig one by one
-            numOfDig++;
-        }
-        int divd= (int)Math.pow(10, numOfDig -1) ;
-        while(num !=0){
-            int ques = num/divd;
-            System.out.println(ques);
+        var sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        var num = sc.nextInt();
 
-            num = num % divd ;
-            divd= divd/10;
-            //lets run this code then we will debug it and convert it into java 17.
-            // so we have got result as expected .
+        var temp = num;
+        var numOfDigits = 0;
 
-            //Lets debug this code and understand whats is happening.
-
+        while (temp != 0) {
+            temp /= 10;
+            numOfDigits++;
         }
 
+        var divisor = (int) Math.pow(10, numOfDigits - 1);
+
+        while (divisor != 0) {
+            var digit = num / divisor;
+            System.out.println(digit);
+
+            num %= divisor;
+            divisor /= 10;
+        }
+
+        sc.close(); // Always good to close resources
     }
-
-
-
-
-
-
 }
